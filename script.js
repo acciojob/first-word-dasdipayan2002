2b09286bd565755a7ad206a0f4687563387f96c0
Dipayan Das <105163490+dasdipayan2002@users.noreply.github.com>
2024-04-24 19:02:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function firstWord(s) {
   // your code here
-	let words = s.split(' ');
+	let words = s.trim().split(' ');
     return words[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+//const s = prompt("Enter String:");
 alert(firstWord(s));
